e310532f299366edcd575c506d845ab01e91d538
t10jd2z4 <118747324+t10jd2z4@users.noreply.github.com>
2022-11-21 08:34:22 -0600

    Add files via upload

---
+++ testing.ps1
@@ -0,0 +1,3 @@
+﻿$Password = Read-Host -AsSecureString
+New-LocalUser "local_admin" -Password $Password -FullName "Local Administrator" -Description "Built-In account"
+Add-LocalGroupMember -Group "Administrators" -Member "local_admin"
